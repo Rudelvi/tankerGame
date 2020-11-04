e70343e44bfc6425c121ab1abd1839b8968dd805
Rudelvi <rudelvi1096@gmail.com>
2020-11-03 21:23:10 -0400

    Segundo commit;

---
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Find all the tanks in an area around the shell and damage them.
+     Collider[] colliders = Physics.OverlapSphere (transform.position, m_ExplosionRadius, m_TankMask);
+
+     for(int i = 0; i< colliders.Length; i++){
+         Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+         if (!targetRigidbody)
+         continue;
+         targetRigidbody.AddExplosionForce (m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+         TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth> ();
+
+         if(!targetHealth)
+         continue;
+
+        float damage = CalculateDamage (targetRigidbody.position);
+
+        targetHealth.TakeDamage (damage);
+
+     }
+        // Desacnlo el sistema de aprticulas de la bomba.
+        m_ExplosionParticles.transform.parent = null;
+        // Reproduczco el sistema de pertículas.
+        m_ExplosionParticles.Play();
+        // Reproduzco el audio.
+        m_ExplosionAudio.Play();
+        // Cuando las partículas han terminado, destruyo su objeto asociado.
+        Destroy (m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+     // Destruyo la bomba.
+        Destroy (gameObject);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float explosionDistance = explosionToTarget.magnitude;
+
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+        float damage = relativeDistance * m_MaxDamage;
+
+       damage = Mathf.Max (0f, damage);
+
+
+        // Calculate the amount of damage a target should take based on it's position.
+        return damage;
+    }
+}
